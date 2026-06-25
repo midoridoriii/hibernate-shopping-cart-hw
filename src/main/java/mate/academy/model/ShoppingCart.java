a28f09425c80864b7d77669fc0e04f845980b41c
midoridoriii <valissa069@gmail.com>
2026-06-25 18:41:16 +0300

    added ShoppingCart class and implementation

---
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -0,0 +1,36 @@
+package mate.academy.model;
+
+import java.util.List;
+
+public class ShoppingCart {
+    private Long id;
+    private List<Ticket> tickets;
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public ShoppingCart setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public ShoppingCart setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+        return this;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public ShoppingCart setUser(User user) {
+        this.user = user;
+        return this;
+    }
+}
