3ef092aeb38e1a356beda1be8a70c7f7fbc4f328
midoridoriii <valissa069@gmail.com>
2026-06-25 18:43:21 +0300

    added annotations in Ticket and ShoppingCart classes

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -1,36 +1,49 @@
 package mate.academy.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.util.List;
 
+@Entity
+@Table(name = "shopping_carts")
 public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToMany
     private List<Ticket> tickets;
+
+    @OneToOne(fetch = FetchType.LAZY)
     private User user;
 
     public Long getId() {
         return id;
     }
 
-    public ShoppingCart setId(Long id) {
+    public void setId(Long id) {
         this.id = id;
-        return this;
     }
 
     public List<Ticket> getTickets() {
         return tickets;
     }
 
-    public ShoppingCart setTickets(List<Ticket> tickets) {
+    public void setTickets(List<Ticket> tickets) {
         this.tickets = tickets;
-        return this;
     }
 
     public User getUser() {
         return user;
     }
 
-    public ShoppingCart setUser(User user) {
+    public void setUser(User user) {
         this.user = user;
-        return this;
     }
 }
--- src/main/java/mate/academy/model/Ticket.java
+++ src/main/java/mate/academy/model/Ticket.java
@@ -1,34 +1,47 @@
 package mate.academy.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tickets")
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private MovieSession movieSession;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
     public Long getId() {
         return id;
     }
 
-    public Ticket setId(Long id) {
+    public void setId(Long id) {
         this.id = id;
-        return this;
     }
 
     public MovieSession getMovieSession() {
         return movieSession;
     }
 
-    public Ticket setMovieSession(MovieSession movieSession) {
+    public void setMovieSession(MovieSession movieSession) {
         this.movieSession = movieSession;
-        return this;
     }
 
     public User getUser() {
         return user;
     }
 
-    public Ticket setUser(User user) {
+    public void setUser(User user) {
         this.user = user;
-        return this;
     }
 }
