1991064c0b5323c985bd5b168dcf6b5cee187816
midoridoriii <valissa069@gmail.com>
2026-06-25 18:39:55 +0300

    added Ticket class and implementation

---
+++ src/main/java/mate/academy/model/Ticket.java
@@ -0,0 +1,34 @@
+package mate.academy.model;
+
+public class Ticket {
+    private Long id;
+    private MovieSession movieSession;
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Ticket setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public MovieSession getMovieSession() {
+        return movieSession;
+    }
+
+    public Ticket setMovieSession(MovieSession movieSession) {
+        this.movieSession = movieSession;
+        return this;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Ticket setUser(User user) {
+        this.user = user;
+        return this;
+    }
+}
